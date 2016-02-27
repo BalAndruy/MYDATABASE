a9b285f63d10dce6c3d65f1f5187aa5b8887ccec
BalAndruy <baladr@ukr.net>
2016-02-27 14:02:39 +0200

    Settings

--- DataBase/DataBase/Form1.cs
+++ DataBase/DataBase/Form1.cs
@@ -7,6 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data.Common; 
 
 namespace DataBase
 {
@@ -15,6 +22,11 @@ namespace DataBase
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
