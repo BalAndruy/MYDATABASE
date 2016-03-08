e76706d246a151374a24c9d439b3e9bfbff81803
BalAndruy <baladr@ukr.net>
2016-03-08 14:36:25 +0200

    Щось

--- DataBase/DataBase/Form4.cs
+++ DataBase/DataBase/Form4.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SQLite;
 namespace DataBase
 {
     public partial class Form4 : Form
